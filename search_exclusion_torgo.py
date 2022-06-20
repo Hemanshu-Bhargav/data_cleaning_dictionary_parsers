50544fa808b6e33127395c2049f83c7fb157f596
Hemanshu Bhargav <56455328+Hemanshu-Bhargav@users.noreply.github.com>
2022-06-20 11:47:37 -0400

    Add files via upload

--- search_exclusion_torgo.py
+++ search_exclusion_torgo.py
@@ -20,35 +20,25 @@ def file_test():
             print(list_item)
             if to_exclude1 in list_item:
                 print("String match successful")
-            # Delete using JSON
-            # #delete myjsonobj['speakerID'];
-
             if to_exclude2 in list_item:
                 print("String match 2 successful")
 
 
-#----------------------------------------------------------------------------
 def json_cleaner():
     with open("valid.json", "r+") as f:
         json_to_dictionary = json.load(f)
-
         for speakerID in json_to_dictionary.keys():
             for inner_entry in json_to_dictionary[speakerID].values():
                 #print(type(inner_entry))
                 #print(inner_entry)
-                if inner_entry == type(str):
+                #for entry_value in inner_entry:
+                if isinstance(inner_entry, str):
                     if to_exclude1 in inner_entry:
+                        print(str(speakerID))
                         json_to_dictionary.pop(speakerID)
                     if to_exclude2 in inner_entry:
+                        print(str(speakerID))
                         json_to_dictionary.pop(speakerID)
-            '''
-            if to_exclude1 in list_item:
-                print("String match successful")
-            # #delete myjsonobj['speakerID'];
-
-            if to_exclude2 in list_item:
-                print("String match 2 successful")
-            '''
 
 
 json_cleaner()
